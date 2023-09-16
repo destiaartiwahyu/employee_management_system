74884bc680846feb701b3e754108fa998c4ab6ff
destiaartiwahyu <destiaartiwahyu@gmail.com>
2023-09-16 21:02:18 +0700

    migration

--- app/Models/PositionUser.php
+++ app/Models/PositionUser.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class PositionUser extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'position_id',
+        'user_id'
+    ];
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,37 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
         },
     },
-
-    plugins: [require('@tailwindcss/forms')],
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+        }
+    },
+    purge: {
+        content: [
+            './app/**/*.php',
+            './resources/**/*.html',
+            './resources/**/*.js',
+            './resources/**/*.jsx',
+            './resources/**/*.ts',
+            './resources/**/*.tsx',
+            './resources/**/*.php',
+            './resources/**/*.vue',
+            './resources/**/*.twig',
+        ],
+        options: {
+            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 };
